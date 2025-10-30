76267fab0b719474cd6ee0c891e3efc68e3f0b41
aidangillespie52 <142521298+aidangillespie52@users.noreply.github.com>
2025-10-29 22:54:49 -0500

    got rid of single implementation for a rule and added candidate implementation instead

--- rules.py
+++ rules.py
@@ -6,105 +6,48 @@ from typing import Tuple
 import numpy as np
 from board import Board
 from typing import Optional
+from itertools import product
 
 @dataclass
 class Result:
     idx: Tuple[int, int]
     value: int
-    
-def single_missing(bd: Board) -> Result:
-    """
-    Looks for a single missing value within either the row, column or box,
-    and returns a result object that has the index
-    
-    Example:
-    12345.789 -> 6 would be the missing in the row
-    """
-    def check_empty(arr: np.array, x: int) -> Optional[Result]:
-        num_missing = bd.count_empty(arr)
-        
-        if num_missing != 1:
-            return None
-        
-        missing_digit_arr = np.setdiff1d(bd.allowed_values(), arr)
-        
-        if len(missing_digit_arr) != 1:
-            raise ValueError("the board is invalid")
-        
-        y = int(np.where(arr == 0)[0][0])
-        missing_digit = int(missing_digit_arr[0])
-        
-        return Result((x,y), missing_digit)
 
-    # check rows and cols
-    for i in range(bd.size):
-        row = bd.get_row(i)
-        col = bd.get_col(i)
-
-        # row
-        res = check_empty(row, i)
-        if res:
-            return res
-        
-        # col
-        res = check_empty(col, i)
-        if res:
-            res.idx = res.idx[::-1]
-            return res
+def find_single_candidates(bd: Board) -> Optional[Result]:
+    cds = bd.get_candidates()
+    pairs = product(range(bd.size), range(bd.size))
     
-    for i in range(bd.num_cells):
-        cell = bd.get_cell(i)
-        res = check_empty(cell, i)
-        if not res:
+    for x,y in pairs:
+        if bd[x,y] != 0:
             continue
         
-        _, arr_idx = res.idx 
-        x,y = bd.cell_index_to_board_index(i, arr_idx)
-        res.idx = (x,y)
-        return res
+        possible_vals = cds[x][y] 
+        
+        if len(possible_vals) != 1:
+            continue
+        return Result((x,y), possible_vals[0])
     
+    return None
+
 if __name__ == '__main__':
-    print("Test col:")
     b = Board(9,3,3)
-    r = np.array(b.allowed_values())
-    np.random.shuffle(r)
-    
-    b[2, :] = r
-    np.random.shuffle(r)
-    b[:, 5] = r
-    b[3,5] = 0
-    
+    b.read("sample.txt")
     print(b)
-    res = single_missing(b)
-    print(res)
-
-    print("Test row:")
-    b = Board(9,3,3)
-    r = np.array(b.allowed_values())
-    np.random.shuffle(r)
     
-    b[3, :] = r
-    np.random.shuffle(r)
-    b[5, :] = r
-    b[5,3] = 0
+    r = True
+    i = 0
+    while r:
+        i += 1
+        r = find_single_candidates(b)
+        x,y = r.idx
+        b[x,y] = r.value
+        
+        if b.count_empty(b._grid) == 0:
+            print("\n")
+            print(f"SOLVED!\n")
+            print(b)
+            break
     
-    print(b)
-    res = single_missing(b)
-    print(res)
-
-    print("Test cell:")
-    b = Board(9,3,3)
-    b[3,0] = 9
-    b[3,1] = 5
-    b[3,2] = 3
-    b[4,0] = 1
-    b[4,1] = 4
-    b[4,2] = 6
-    b[5,0] = 7
-    b[5,1] = 8
-    b[5,2] = 0
-
-    print(b)
-    res = single_missing(b)
-    print(res)
+    
+    
 
